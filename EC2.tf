68bf2e5d57fcb7b92992cc4d052e9fc03be6c85d
obynodavid12 <obynodavid12@gmail.com>
2022-03-27 22:00:42 -0400

    just committing

---
+++ EC2.tf
@@ -0,0 +1,56 @@
+## Random
+resource "dev-project" "sg" {}
+
+## AWS VPC
+resource "aws_vpc" "awsec2demo-vpc" {
+  cidr_block = "172.16.0.0/16"
+  tags = {
+    Name = "vpc-dataalgebra"
+  }
+}
+## AWS VPC Subnet
+resource "aws_subnet" "awsec2demo" {
+  vpc_id            = aws_vpc.awsec2demo.id
+  cidr_block        = "172.16.10.0/24"
+
+  tags = {
+    Name = "subnet-dataalgebra"
+  }
+}
+
+## AWS Network Interface
+resources "aws_network_interface" "awsec2demo" {
+  subnet_id   = aws_subnet.awsec2demo.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "NI-dataalgebra"
+  }
+}
+
+## AWS Security Group
+resource "aws_security_group" "awsec2demo" {
+  name = "${random_dev-project.id}-sg"
+  vpc_id      = aws_vpc.awsec2demo.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["68.83.212.136/32"]
+
+  tags = {
+    Name         = "allow-ssh"
+
+  }
+}
+
+## AWS EC2
+resource "aws_instance" "awsec2demo" {
+  ami           = "ami-015848f7d63c649a5"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.awsec2demo.id
+    device_index         = 0
+  }
+}
---
+++ Main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    random = {
+      source  = "hashicorp/random"
+    }
+  }
+
+  cloud {
+  organization = "AWS-DataalgebraCloud"
+
+    workspace {
+      name = "AWS-DataalgebraCloud"
+    }
+  }
+}
+
+## random provider
+provider "random" {}
+
+## Provider us-east-2
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+
+
+## backend data for terraform -this was replaced in the Main.tf file when we use terraform cloud
+# terraform {
+#   # Terraform version at the time of writing this script
+#   required_version = ">= 0.12.24"
+
+#   backend "s3" {
+#      bucket = "dataalgebrabackendtf"
+#      key    = "quickdataalgebrabackend_2.tfstate"
+#      region = "us-east-2"
+#   }
+# }
+
+# ## random provider
+# provider "random" {}
+
+# ## Provider us-east-2
+# provider "aws" {
+#   region = "us-east-2"
+#}
