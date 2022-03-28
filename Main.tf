d02409e5b416c5df82072639bd293f221e82120e
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-03-28 13:47:40 -0400

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,18 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      #version = "~> 3.63"
+      source = "hashicorp/aws"
     }
     random = {
-      source = "hashicorp/aws"
-    #  version = ">=3.0.0"
-    }     
+      source = "hashicorp/random"
+    }
   }
-  
-  cloud {
-  organization = "Dataalgebra-Cloud"
 
+  cloud {
+    organization = "Dataalgebra-Cloud"
     workspaces {
       name = "AWS-DataalgebraCloud"
     }
@@ -34,22 +31,10 @@ provider "aws" {
 
 
 
-## backend data for terraform -this was replaced in the Main.tf file when we use terraform cloud
-# terraform {
-#   # Terraform version at the time of writing this script
-#   required_version = ">= 0.12.24"
 
-#   backend "s3" {
-#      bucket = "dataalgebrabackendtf"
-#      key    = "quickdataalgebrabackend_2.tfstate"
-#      region = "us-east-2"
-#   }
-# }
 
-# ## random provider
-# provider "random" {}
 
-# ## Provider us-east-2
-# provider "aws" {
-#   region = "us-east-2"
-#}
+
+
+
+
