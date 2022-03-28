c9fa5b6f716095286151120e349e79d26c783752
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-03-28 14:45:23 -0400

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,36 +1,32 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.27"
-      
+      source  = "hashicorp/aws"
+      version = "~> 3.63"
     }
+    random = {
+      source = "hashicorp/aws"
+      version = ">=3.0.0"
+    }     
   }
-  required_version = ">= 0.14.9"
-
-}
+  
   cloud {
-    organization = "Dataalgebra-Cloud"
+  organization = "Dataalgebra-Cloud"
+
     workspaces {
       name = "AWS-DataalgebraCloud"
     }
   }
+}
 
 ## random provider
 provider "random" {}
 
 ## Provider us-east-2
 provider "aws" {
-    version = "~> 0.13.5"
-    region  = "us-east-2"
-    profile = "kopsuser"
+  region = "us-east-2"
+  profile = "kopsuser"
 }
-
-
-
-
-
-
 
 
 
