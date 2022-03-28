2ef913e97a875722d4e84e826adee29b89aaba82
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-03-28 13:57:20 -0400

    aws-alternate

--- Main.tf
+++ Main.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 2.7.0"
+      configuration_aliases = [ aws.alternate ]
     }
     random = {
       source = "hashicorp/random"
