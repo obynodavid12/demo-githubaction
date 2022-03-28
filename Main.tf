2c468405121c0f2e89b0fefdcd383536926fc846
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-03-28 12:21:17 -0400

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -9,7 +9,7 @@ terraform {
       version = ">=3.0.0"
     }     
   }
-  required_version = "~> 1.1.7"
+  
   cloud {
   organization = "Dataalgebra-Cloud"
 
@@ -24,6 +24,7 @@ provider "random" {}
 
 ## Provider us-east-2
 provider "aws" {
+  version = ">= 3.53.0"
   region = "us-east-2"
 }
 
